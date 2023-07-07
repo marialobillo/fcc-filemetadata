c13a7febc81025b9a5fbd083634875a10dc3af22
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-07-07 11:56:16 +0200

    feat: add post route for upload files

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
-var app = express();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// upload single file and get file info
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    res.status(200).json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size,
+    });
+});
 
-
-
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log("Your app is listening on port " + port);
 });
